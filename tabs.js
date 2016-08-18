c7d5e936dcdc9032098f203029b537677e53e1eb
Billy Everyteen <Billy Everyteen>
2016-08-18 05:25:08 +0300

    g

--- tabs.js
+++ tabs.js
@@ -1,15 +1,21 @@
-﻿
-angular.module('my', ['ui.tree']).controller('TodoCtrl',
+﻿angular.module('my', ['ui.tree']).controller('TodoCtrl',
     function ($scope, $timeout, $filter) {
-        storage = chrome.storage.local;
+
+        var storage = chrome.storage.local;
         getTabs();
         chrome.tabs.onRemoved.addListener(function (tabid) {
-            var tab = Enumerable.From($scope.windows).SelectMany(function (a) { return a.tabs;}).First(function (a) {return a.id == tabid;});
+            var tab = Enumerable.From($scope.windows).SelectMany(function (a) {
+                return a.tabs;
+            }).First(function (a) {
+                return a.id == tabid;
+            });
 
-            if(!tab.url.startsWith("chrome-extension") && !skipSave && !$scope.savedWindows[0].tabs.contains(tab,function (a, b) { return a.url == b.url;})) {
-                $scope.savedWindows[$scope.savedWindows.length - 1].tabs.push(clone(tab));
-                storage.set({windows: $scope.savedWindows});
-                skipSave =false;
+            if (!tab.url.startsWith("chrome-extension") && !skipSave && !$scope.folders.windows[0].tabs.contains(tab, function (a, b) {
+                    return a.url == b.url;
+                })) {
+                $scope.folders.windows[$scope.folders.windows.length - 1].tabs.push(clone(tab));
+                storage.set({windows: $scope.folders.windows});
+                skipSave = false;
             }
             //$scope.closedWindows.push();
             getTabs();
@@ -17,40 +23,46 @@ angular.module('my', ['ui.tree']).controller('TodoCtrl',
         chrome.tabs.onUpdated.addListener(function (tab) {
             getTabs();
         })
-        $scope.toggle2 = function (scope,item) {
+        $scope.selectFolder = function(node)
+        {
+            console.log(node)
+            $scope.selectedFolder = node;
+
+        }
+        $scope.toggle2 = function (scope, item) {
             item.collapsed = !item.collapsed;
             //scope.toggle();
             Save();
         };
-        
-  $scope.remove = function (scope) {
-        scope.remove();
-      };
 
-      
-         $scope.newSubItem = function (scope) {
-             console.log(scope);
-        var nodeData = scope.$modelValue;
-        nodeData.nodes.push({nodes: [],windows:[]});
-      };
-      
-        
+        $scope.remove = function (scope) {
+            scope.remove();
+        };
+
+
+        $scope.newSubItem = function (scope) {
+            console.log(scope);
+            var nodeData = scope.$modelValue;
+            nodeData.nodes.push({nodes: [], windows: []});
+        };
+
+
         $scope.treeOptions = {
-            dropped : function (e) {
+            dropped: function (e) {
                 Save();
                 getTabs();
             },
-            beforeDrop : function (e) {
+            beforeDrop: function (e) {
 
                 var item = e.source.nodeScope.$modelValue;
                 var alist = e.source.nodesScope.$modelValue;
                 var blist = e.dest.nodesScope.$modelValue;
-                if(item.recentlyClosed)return; 
-                $scope.savedWindows.savedWindows=true;
-                $scope.savedWindows.forEach(function (a) {
-                    a.tabs.savedTabs=true;
+                if (item.recentlyClosed)return;
+                $scope.selectedFolder.windows.folders.windows = true;
+                $scope.selectedFolder.windows.forEach(function (a) {
+                    a.tabs.savedTabs = true;
                 })
-                $scope.windows.activeWindows=true;
+                $scope.windows.activeWindows = true;
                 $scope.windows.forEach(function (a) {
                     a.tabs.activeTabs = true;
                 })
@@ -64,25 +76,25 @@ angular.module('my', ['ui.tree']).controller('TodoCtrl',
 
                 if (alist.activeTabs) {
                     if (blist.savedTabs) {
-                        blist.splice(e.dest.index,0, clone(item));
+                        blist.splice(e.dest.index, 0, clone(item));
                         chrome.tabs.remove(item.id);
-                        
+
                     }
                 }
-                 Save();
-                return alist==blist;
+                Save();
+                return alist == blist;
             }
         }
 
         $scope.GetIcon = function (tab) {
-            return  tab.favIconUrl && tab.favIconUrl.startsWith("http")?  tab.favIconUrl :'https://www.google.com/images/icons/product/ebooks-16.png';
+            return tab.favIconUrl && tab.favIconUrl.startsWith("http") ? tab.favIconUrl : 'https://www.google.com/images/icons/product/ebooks-16.png';
         };
 
         $scope.CloseTab = function (tab) {
             if (tab.id)
                 chrome.tabs.remove(tab.id);
             else
-                $scope.savedWindows.forEach(function (win) {
+                $scope.selectedFolder.windows.forEach(function (win) {
                     Remove(win.tabs, tab);
                     Save();
                 })
@@ -98,12 +110,12 @@ angular.module('my', ['ui.tree']).controller('TodoCtrl',
             }
             else {
                 chrome.tabs.update(tab.id, {selected: true});
-                chrome.windows.update(tab.windowId,{focused:true})
+                chrome.windows.update(tab.windowId, {focused: true})
             }
         }
 
         $scope.CloseWindow = function (window) {
-            if(window.id)
+            if (window.id)
                 chrome.windows.remove(window.id);
             else
                 RemoveWindow(window);
@@ -118,29 +130,36 @@ angular.module('my', ['ui.tree']).controller('TodoCtrl',
             RemoveWindow(window);
         }
         var skipSave;
-        $scope.SaveWindow = function (window,into,pos) {
-            if(!into)
+        $scope.SaveWindow = function (window, into, pos) {
+            if (!into)
                 var win = {tabs: [], date: new Date().toISOString().slice(0, 20)};
 
             window.tabs.forEach(function (tab) {
                 if (!tab.url.startsWith("chrome-extension")) {
-                    skipSave=true;
+                    skipSave = true;
                     chrome.tabs.remove(tab.id);
-                    if(into)
+                    if (into)
                         into.splice(pos++, 0, clone(tab))
                     else
                         win.tabs.push(clone(tab));
 
                 }
             });
-            if(!into)
+            if (!into)
                 AddWindow(win);
         }
 
         function clone(tab) {
-            return {favIconUrl: tab.favIconUrl, title: tab.title, url: tab.url, rand: Math.floor((Math.random() * 1000) + 1)};
+            return {
+                favIconUrl: tab.favIconUrl,
+                title: tab.title,
+                url: tab.url,
+                rand: Math.floor((Math.random() * 1000) + 1)
+            };
         }
+
         var promise;
+
         function getTabs() {
 
             $timeout.cancel(promise);
@@ -149,52 +168,53 @@ angular.module('my', ['ui.tree']).controller('TodoCtrl',
                     $scope.windows = windows;
 
                     storage.get('folders', function (storage) {
-                        $scope.folders = storage.folders||{nodes:[],windows:[],title:"default"};
+                        $scope.folders = storage.folders || {nodes: [], windows: [], title: "default"};
                         $scope.folders2 = [$scope.folders];
-                        $scope.savedWindows = $scope.folders.windows;
-                        console.log($scope.folders) 
-                        if(!$scope.savedWindows[0])
-                        {
-                            $scope.savedWindows[0] = {tabs:[]};
-                            $scope.savedWindows[0].recentlyClosed = true;
-                            $scope.savedWindows[0].name ="closed";
+                        if(!$scope.selectedFolder)
+                            $scope.selectedFolder = $scope.folders;
+                        console.log($scope.folders)
+                        if (!$scope.folders.windows[0]) {
+                            $scope.folders.windows[0] = {tabs: []};
+                            $scope.folders.windows[0].recentlyClosed = true;
+                            $scope.folders.windows[0].name = "closed";
                         }
                         Refresh();
                     });
                 });
             }, 100);
         }
+
         function Save() {
             storage.set({folders: $scope.folders});
         }
+
         function Refresh() {
             $scope.$apply();
         }
 
         function RemoveWindow(win) {
-            if($scope.savedWindows.indexOf(win) == 0)
+            if ($scope.selectedFolder.windows.indexOf(win) == 0)
                 win.tabs = [];
             else
-                Remove($scope.savedWindows, win);
+                Remove($scope.selectedFolder.windows, win);
             Save();
             getTabs();
         }
 
-        $scope.Save =function() {
+        $scope.Save = function () {
             Save();
         }
-       
+
 
         function AddWindow(win, remove) {
-            $scope.savedWindows.splice(0,0,win);
+            $scope.selectedFolder.windows.splice(0, 0, win);
             Save();
             getTabs();
         }
 
 
-
-    }).filter('reverse', function() {
-  return function(items) {
-    return items.slice().reverse();
-  };
+    }).filter('reverse', function () {
+    return function (items) {
+        return items.slice().reverse();
+    };
 });
